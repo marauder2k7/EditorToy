e3c77c57eac7ec4b28c8c5ea8700db9865429814
marauder2k7 <ctrl-intelligence@live.co.uk>
2020-03-15 10:26:22 +0000

    DeleteUnnecessary

--- 1/projects/GameTest/1/assets/scenes/GameScene.prefs.cs
+++
@@ -1,6 +0,0 @@
-$pref::Scene::CameraX = "16";
-$pref::Scene::CameraY = "9";
-$pref::Scene::GravityX = "0";
-$pref::Scene::GravityY = "-9.80000019";
-$pref::Scene::PositionIterations = "3";
-$pref::Scene::VelocityIterations = "8";
